6e1a5d9d0a154f4a2a389f448074c6bc09f3300d
Russell Strauss <rstrauss@agencyoasis.com>
2016-11-17 11:05:54 -0500

    Adding constants file and making nodes expand on hover

---
+++ assets/js/constants.js
@@ -0,0 +1,24 @@
+consts = (function () {
+
+	nodes = {
+		DEFAULT_RADIUS: 50,
+		EXPANDED_RADIUS: 500,
+		EXPAND_PROXIMITY: 100
+	},
+	squares = {
+
+	}
+
+
+	/**
+	 * Public API.
+	 */
+
+	return {
+
+		nodes: nodes,
+		squares: squares
+
+	}
+
+})(window.consts, document, window);
